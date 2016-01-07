c5030f274284e15236c1fa4f4101cf46fa2105f8
= <kingliin814@gmail.com>
2016-01-07 18:31:01 +0800

    chapter3 add contact views and setup base_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@sitle = "Ruby on Rails Tutorial Sample App"
+	end
+
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@sitle}"
 
   end
 
   test "should get help" do
     post :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@sitle}"
   end
 
    test "should get about" do
     post :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@sitle}"
+  end
+
+    test "should get contact" do
+    post :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@sitle}"
 
   end
 
